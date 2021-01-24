e77a073928742fe0dd6e3623d10aa95764b9b89f
Kensuke-code <kenzo71719083@gmail.com>
2021-01-24 13:43:00 +0900

    log情報変更

--- bitcoin.rb
+++ bitcoin.rb
@@ -7,7 +7,7 @@ MINIMUM_PURCHASE_UNIT = 0.001 #ビットコインの最低購入単位
 
 log = Logger.new('./logfile')
 
-log.info('月次仮想通貨売買を開始します')
+log.info('月次仮想通貨売買を開始')
 
 # 現在の板情報を取得
 price = get_price["mid_price"]
@@ -18,10 +18,12 @@ my_btc = get_balance("BTC")
 my_jpy = get_balance("JPY")
 puts "現在の保有資産(BTC): #{my_btc} btc"
 puts "現在の保有資産(JPY): #{my_jpy} 円"
+log.info("現在の保有資産: BTC:#{my_btc}, JPY:#{my_jpy}")
 
 # 最低購入金額
 minimum_purchase_amount = (price * MINIMUM_PURCHASE_UNIT).ceil
 puts "最低購入金額は #{minimum_purchase_amount} 円"
+log.info("最低購入金額 #{minimum_purchase_amount}")
 
 # 購入する
 puts "購入処理を行います"
@@ -30,13 +32,16 @@ sleep(1)
 # もし、現在の資産価格(円)が足りなければ、エラーを通知
 if my_jpy < minimum_purchase_amount
     puts "残高不足です。注文を中止します。"
+    log.error('注文失敗(残高不足)')
 else 
     begin
         buy_order
         puts "注文が完了しました"
+        log.info('注文成功')
     rescue Exeption => e
         puts "注文に失敗しました"
         puts e.class
+        log.info("注文失敗 #{e.class}")
     end
 end
 
